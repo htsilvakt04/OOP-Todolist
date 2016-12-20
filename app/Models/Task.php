e00be9627dc0c5e1c08cd5cf87891eb64ea7fff3
Trung-Silva <htsilvakt04@gmail.com>
2016-12-20 22:40:05 +0700

    Finish creating Task Class

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,8 +1,46 @@
 <?php
 // Using this Model to....
 namespace Todo\Models;
-
+use DateTime;
 class Task
 {
-
+  protected $id;
+  protected $description;
+  protected $due;
+  protected $complete = false;
+  public function __construct()
+  {
+    echo "Yolo, you're accessing the Task class";
+  }
+  public function setDescription($description)
+  {
+    $this->description = $description;
+  }
+  public function getDescription()
+  {
+    return $this->description;
+  }
+  public function setComplete($complete = true)
+  {
+    $this->complete = $complete;
+  }
+  public function getComplete()
+  {
+    return (bool)$this->complete;
+  }
+  public function setDue(DateTime $due)
+  {
+    $this->due = $due;
+  }
+  public function getDue()
+  {
+    if(!$this->due instanceof DateTime) {
+      return new DateTime($this->due);
+    }
+    return $this->due;
+  }
+  public function getId()
+  {
+    return $this->id;
+  }
 }
--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
-
+use Todo\Models\Task;
 require("vendor/autoload.php");
+
+
+$task = new Task;
+$task->setDescription("Finish this course");
+$task->setDue(new DateTime("+2 day",new DateTimeZone('Asia/Ho_Chi_Minh')));
+
+var_dump($task);
